86ff5b16cf87ec2c3e349ba600ef9e865e64d79c
Shengfeng Li <cs.shengfeng.li@gmail.com>
2018-10-04 16:20:16 -0500

    Add Board class

---
+++ game.py
@@ -0,0 +1,123 @@
+from __future__ import print_function
+import numpy as np
+
+
+class Board(object):
+    """board for the game"""
+
+    def __init__(self, width=8, height=8, n_in_row=5, start_player=0):
+        self.width = width
+        self.height = height
+        # need how many pieces in a row to win
+        self.n_in_row = n_in_row
+        if self.width < self.n_in_row or self.height < self.n_in_row:
+            raise Exception('board width and height can not be '
+                            'less than {}'.format(self.n_in_row))
+        self.states = {}
+        # Two players
+        self.players = [1, 2]  # player1 and player2
+        self.current_player = self.players[start_player]  # start player
+        # keep available moves in a list
+        self.available_location = list(range(self.width * self.height))
+        self.last_move = -1
+
+    def move_to_location(self, move):
+        """
+        6*6 board's moves like:
+        30 31 32 33 34 35
+        24 25 26 27 28 29
+        18 19 20 21 22 23
+        12 13 14 15 16 17
+         6  7  8  9 10 11
+         0  1  2  3  4  5
+        and move 10 will make location (4,1)
+        """
+        w = move % self.width
+        h = move // self.width
+        return [h, w]
+
+    def location_to_move(self, location):
+        if len(location) != 2:
+            return -1
+        h = location[0]
+        w = location[1]
+        move = h * self.width + w
+        if move not in range(self.width * self.height):
+            return -1
+        return move
+
+    def current_state(self):
+        """return the board state from the perspective of the current player.
+        state shape: 4*width*height
+        """
+
+        square_state = np.zeros((4, self.width, self.height))
+        if self.states:
+            moves, players = np.array(list(zip(*self.states.items())))
+            move_curr = moves[players == self.current_player]
+            move_oppo = moves[players != self.current_player]
+            square_state[0][move_curr // self.width,
+                            move_curr % self.height] = 1.0
+            square_state[1][move_oppo // self.width,
+                            move_oppo % self.height] = 1.0
+            # indicate the last move location
+            square_state[2][self.last_move // self.width,
+                            self.last_move % self.height] = 1.0
+        if len(self.states) % 2 == 0:
+            square_state[3][:, :] = 1.0  # indicate the colour to play
+        return square_state[:, ::-1, :]
+
+    def do_move(self, move):
+        self.states[move] = self.current_player
+        self.available_location.remove(move)
+        self.current_player = (
+            self.players[0] if self.current_player == self.players[1]
+            else self.players[1]
+        )
+        self.last_move = move
+
+    def has_a_winner(self):
+        width = self.width
+        height = self.height
+        states = self.states
+        n = self.n_in_row
+
+        moved = list(set(range(width * height)) - set(self.available_location))
+        if len(moved) < self.n_in_row + 2:
+            return False, -1
+
+        for m in moved:
+            h = m // width
+            w = m % width
+            player = states[m]
+
+            if (w in range(width - n + 1) and
+                    len(set(states.get(i, -1) for i in range(m, m + n))) == 1):
+                return True, player
+
+            if (h in range(height - n + 1) and
+                    len(set(states.get(i, -1) for i in range(m, m + n * width, width))) == 1):
+                return True, player
+
+            if (w in range(width - n + 1) and h in range(height - n + 1) and
+                    len(set(states.get(i, -1) for i in range(m, m + n * (width + 1), width + 1))) == 1):
+                return True, player
+
+            if (w in range(n - 1, width) and h in range(height - n + 1) and
+                    len(set(states.get(i, -1) for i in range(m, m + n * (width - 1), width - 1))) == 1):
+                return True, player
+
+        return False, -1
+
+    def game_end(self):
+        """Check whether the game is ended or not"""
+        win, winner = self.has_a_winner()
+        if win:
+            return True, winner
+        elif not len(self.available_location):
+            return True, -1
+        return False, -1
+
+    def get_current_player(self):
+        return self.current_player
+
