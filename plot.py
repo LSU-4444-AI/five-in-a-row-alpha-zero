387f781c48a946a2d0474a2d5828f11aa124db97
Shengfeng Li <cs.shengfeng.li@gmail.com>
2018-11-21 23:18:15 -0600

    Update plot.py

--- plot.py
+++ plot.py
@@ -6,10 +6,11 @@ from keras.callbacks import History
 from policy_value_net_keras import his_path
 from keras.utils import plot_model
 
+
 # Model accuracy and loss plots
 def plot_model_history(model_details):
     # Create sub-plots
-    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+    # fig, axs = plt.subplots(1, 2, figsize=(15, 5))
 
     # Summarize history for accuracy
     # axs[0].plot(range(1, len(model_details.history['acc']) + 1),
@@ -24,30 +25,49 @@ def plot_model_history(model_details):
     # axs[0].legend(['train', 'val'], loc='best')
 
     # Summarize history for loss
-    axs[0].plot(range(1, len(model_details.history['loss']) + 1),
-                model_details.history['loss'])
+    # axs[1].plot(range(1, len(model_details.history['loss']) + 1),
+    #             model_details.history['loss'])
     # axs[0].plot(range(1, len(model_details.history['val_loss']) + 1),
     #             model_details.history['val_loss'])
-    axs[0].set_title('Model Loss')
-    axs[0].set_ylabel('Loss')
-    axs[0].set_xlabel('Epoch')
-    axs[0].set_xticks(np.arange(1, len(model_details.history['loss']) + 1),
-                      len(model_details.history['loss']) / 10)
-    axs[0].legend(['train', 'loss'], loc='best')
+    # axs[1].set_title('Model Loss')
+    # axs[1].set_ylabel('Loss')
+    # axs[1].set_xlabel('Epoch')
+    # axs[1].set_xticks(np.arange(1, len(model_details.history['loss']) + 1),
+    #                   len(model_details.history['loss']) / 10)
+    # axs[1].legend(['train', 'loss'], loc='best')
+
+    # Summarize history for loss
+    plt.plot(range(1, len(model_details.history['loss']) + 1),
+             model_details.history['loss'])
+    # axs[0].plot(range(1, len(model_details.history['val_loss']) + 1),
+    #             model_details.history['val_loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    # plt.xticks(np.arange(1, len(model_details.history['loss']) + 1),
+    #            len(model_details.history['loss']) / 10)
+    plt.legend(['train', 'loss'], loc='best')
     plt.show()
 
 
-# Plot saved model history
-if os.path.exists(his_path):
-    # Save model history
-    with open(his_path, 'rb') as file_pi:
-        model_history = History()
-        model_history.history = pickle.load(file_pi)
-        plot_model_history(model_history)
-else:
-    raise ValueError('No model history found')
+def plot(his_paths):
+    # Plot saved model history
+    for history_path in his_paths:
+        if os.path.exists(history_path):
+            # Save model history
+            with open(history_path, 'rb') as file_pi:
+                model_history = History()
+                model_history.history = pickle.load(file_pi)
+                plot_model_history(model_history)
+        else:
+            raise ValueError('No model history found')
+
 
 # plot model by keras. pydot is required
 # model = im.get_model()
 # plot_model(model, to_file='./Doc/model.png')
 
+
+if __name__ == '__main__':
+    plot(['./saved/model_6307_11782_history',
+          './saved/model_22221_27518_history'])
